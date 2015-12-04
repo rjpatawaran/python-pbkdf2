3300b032ca1f5d1361c93f775248570762981718
Firemark <marpiechula@gmail.com>
2015-12-04 14:14:24 +0100

    support py3

--- pbkdf2.py
+++ pbkdf2.py
@@ -42,86 +42,137 @@
 """
 import hmac
 import hashlib
+import sys
+import codecs
 from struct import Struct
 from operator import xor
-from itertools import izip, starmap
 
+py_ver = sys.version_info[0]
+
+if py_ver == 2:
+    from itertools import izip, starmap
+    can_encode = lambda s: isinstance(s, unicode)
+elif py_ver == 3:
+    from itertools import starmap
+    izip = zip
+    xrange = range
+    can_encode = lambda s: isinstance(s, str)
+else:
+    raise RuntimeError('unknown python version')
 
 _pack_int = Struct('>I').pack
 
 
-def pbkdf2_hex(data, salt, iterations=1000, keylen=24, hashfunc=None):
+def pbkdf2_hex(
+    data, salt, iterations=1000, keylen=24, hashfunc=None, encoding='utf-8'):
     """Like :func:`pbkdf2_bin` but returns a hex encoded string."""
-    return pbkdf2_bin(data, salt, iterations, keylen, hashfunc).encode('hex')
+    result = pbkdf2_bin(
+        data, salt, iterations, keylen, hashfunc, encoding='utf-8'
+    )
+
+    return codecs.encode(result, 'hex')
 
 
-def pbkdf2_bin(data, salt, iterations=1000, keylen=24, hashfunc=None):
+def pbkdf2_bin(
+    data, salt, iterations=1000, keylen=24, hashfunc=None, encoding='utf-8'):
     """Returns a binary digest for the PBKDF2 hash algorithm of `data`
     with the given `salt`.  It iterates `iterations` time and produces a
     key of `keylen` bytes.  By default SHA-1 is used as hash function,
     a different hashlib `hashfunc` can be provided.
     """
     hashfunc = hashfunc or hashlib.sha1
+    if can_encode(data):
+        data = data.encode(encoding)
+    if can_encode(salt):
+        salt = salt.encode(encoding)
     mac = hmac.new(data, None, hashfunc)
-    def _pseudorandom(x, mac=mac):
+    blocks_len = -(-keylen // mac.digest_size) + 1
+    return _bin(mac, salt, blocks_len, iterations)[:keylen]
+
+
+def _bin_py3(mac, salt, blocks_len, iterations):
+    def _pseudorandom(x):
+        h = mac.copy()
+        h.update(x)
+        return h.digest()
+
+    buf = b""
+    for block in xrange(1, blocks_len):
+        rv = u = _pseudorandom(salt + _pack_int(block))
+        for i in xrange(iterations - 1):
+            u = _pseudorandom(u)
+            rv = starmap(xor, izip(rv, u))
+        buf += bytes(rv)
+    return buf
+
+
+def _bin_py2(mac, salt, blocks_len, iterations):
+    def _pseudorandom(x):
         h = mac.copy()
         h.update(x)
         return map(ord, h.digest())
     buf = []
-    for block in xrange(1, -(-keylen // mac.digest_size) + 1):
+    for block in xrange(1, blocks_len):
         rv = u = _pseudorandom(salt + _pack_int(block))
         for i in xrange(iterations - 1):
             u = _pseudorandom(''.join(map(chr, u)))
             rv = starmap(xor, izip(rv, u))
         buf.extend(rv)
-    return ''.join(map(chr, buf))[:keylen]
+    return ''.join(map(chr, buf))
 
+_bin = _bin_py3 if py_ver == 3 else _bin_py2
+
+TEST_TEMPLATE = """
+Test failed:
+ Expected:   %s
+ Got:        %s
+ Parameters:
+  data=%s
+  salt=%s
+  iterations=%d
+"""
 
 def test():
     failed = []
     def check(data, salt, iterations, keylen, expected):
         rv = pbkdf2_hex(data, salt, iterations, keylen)
         if rv != expected:
-            print 'Test failed:'
-            print '  Expected:   %s' % expected
-            print '  Got:        %s' % rv
-            print '  Parameters:'
-            print '    data=%s' % data
-            print '    salt=%s' % salt
-            print '    iterations=%d' % iterations
-            print
+            s = TEST_TEMPLATE % (
+                expected, rv, data, salt, iterations
+            )
+            print(s)
             failed.append(1)
 
     # From RFC 6070
     check('password', 'salt', 1, 20,
-          '0c60c80f961f0e71f3a9b524af6012062fe037a6')
+          b'0c60c80f961f0e71f3a9b524af6012062fe037a6')
     check('password', 'salt', 2, 20,
-          'ea6c014dc72d6f8ccd1ed92ace1d41f0d8de8957')
+          b'ea6c014dc72d6f8ccd1ed92ace1d41f0d8de8957')
     check('password', 'salt', 4096, 20,
-          '4b007901b765489abead49d926f721d065a429c1')
+          b'4b007901b765489abead49d926f721d065a429c1')
     check('passwordPASSWORDpassword', 'saltSALTsaltSALTsaltSALTsaltSALTsalt',
-          4096, 25, '3d2eec4fe41c849b80c8d83662c0e44a8b291a964cf2f07038')
+          4096, 25, b'3d2eec4fe41c849b80c8d83662c0e44a8b291a964cf2f07038')
     check('pass\x00word', 'sa\x00lt', 4096, 16,
-          '56fa6aa75548099dcc37d7f03425e0c3')
+          b'56fa6aa75548099dcc37d7f03425e0c3')
     # This one is from the RFC but it just takes for ages
     ##check('password', 'salt', 16777216, 20,
     ##      'eefe3d61cd4da4e4e9945b3d6ba2158c2634e984')
 
     # From Crypt-PBKDF2
     check('password', 'ATHENA.MIT.EDUraeburn', 1, 16,
-          'cdedb5281bb2f801565a1122b2563515')
+          b'cdedb5281bb2f801565a1122b2563515')
     check('password', 'ATHENA.MIT.EDUraeburn', 1, 32,
-          'cdedb5281bb2f801565a1122b25635150ad1f7a04bb9f3a333ecc0e2e1f70837')
+          b'cdedb5281bb2f801565a1122b25635150ad1f7a04bb9f3a333ecc0e2e1f70837')
     check('password', 'ATHENA.MIT.EDUraeburn', 2, 16,
-          '01dbee7f4a9e243e988b62c73cda935d')
+          b'01dbee7f4a9e243e988b62c73cda935d')
     check('password', 'ATHENA.MIT.EDUraeburn', 2, 32,
-          '01dbee7f4a9e243e988b62c73cda935da05378b93244ec8f48a99e61ad799d86')
+          b'01dbee7f4a9e243e988b62c73cda935da05378b93244ec8f48a99e61ad799d86')
     check('password', 'ATHENA.MIT.EDUraeburn', 1200, 32,
-          '5c08eb61fdf71e4e4ec3cf6ba1f5512ba7e52ddbc5e5142f708a31e2e62b1e13')
+          b'5c08eb61fdf71e4e4ec3cf6ba1f5512ba7e52ddbc5e5142f708a31e2e62b1e13')
     check('X' * 64, 'pass phrase equals block size', 1200, 32,
-          '139c30c0966bc32ba55fdbf212530ac9c5ec59f1a452f5cc9ad940fea0598ed1')
+          b'139c30c0966bc32ba55fdbf212530ac9c5ec59f1a452f5cc9ad940fea0598ed1')
     check('X' * 65, 'pass phrase exceeds block size', 1200, 32,
-          '9ccad6d468770cd51b10e6a68721be611a8b4d282601db3b36be9246915ec82a')
+          b'9ccad6d468770cd51b10e6a68721be611a8b4d282601db3b36be9246915ec82a')
 
     raise SystemExit(bool(failed))
 
